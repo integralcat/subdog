bc000f71026e4a692e2a9bafc7810489bd2084de
c2-07 <114809705+C2-07@users.noreply.github.com>
2026-03-05 02:53:35 +0530

    Removed: errors

--- errors.py
+++
@@ -1,5 +0,0 @@
-class WildcardDNSDetected(Exception):
-    """Target uses wildcard DNS (e.g *.google.com),
-    Hence it will always flag every subdomain as valid
-    even the invalid ones.
-    """
